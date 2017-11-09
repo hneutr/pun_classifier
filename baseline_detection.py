8654aa44248f0f575cbe51809c7ff2a0a1ca18fe
Hunter Wapman <hunter.wapman@gmail.com>
2017-11-09 16:55:07 -0700

    added in baseline for detection (multinomial naive bayes)

--- baseline_detection.py
+++ baseline_detection.py
@@ -1,13 +1,45 @@
-# A baseline pun detection classifier
+import argparse
+import pickle
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 
-class BaselinePunDetectionClassifier:
-    def __init__(self):
-        pass
+SEED = 20171109
 
-    # TODO
-    def train(self, x_train, y_train):
-        pass
+class Data:
+    def __init__(self, path):
+        with open(path, 'rb') as f:
+            x_set, y_set = pickle.load(f)
 
-    # TODO
-    def test(self, x_test):
-        pass
+            max_len = max([len(x) for x in x_set])
+
+            train_x, valid_x, self.train_y, self.valid_y = train_test_split(x_set, y_set, random_state=SEED)
+
+            for x in train_x:
+                while len(x) < max_len:
+                    x.append("")
+
+            for x in valid_x:
+                while len(x) < max_len:
+                    x.append("")
+
+            self.train_x = [ " ".join(x) for x in train_x ]
+            self.valid_x = [ " ".join(x) for x in valid_x ]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='baseline multinomial naive bayes')
+    parser.add_argument('--graphic', type=str, default='homographic', help="which type of pun ['homographic', 'heterographic']. Default: homographic")
+
+    args = parser.parse_args()
+
+    data = Data("data/pickles/test-1-%s.pkl.gz" % args.graphic)
+
+    nb = MultinomialNB()
+    
+    count_vec = CountVectorizer()
+    x_train_counts = count_vec.fit_transform(data.train_x)
+    x_valid_counts = count_vec.transform(data.valid_x)
+
+    nb.fit(x_train_counts, data.train_y)
+    score = nb.score(x_valid_counts, data.valid_y)
+    print(score)
