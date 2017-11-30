fb2c4b4532e51e355ab9ea14bdfca8af771c2cbc
Cathlyn Stone <cathlyn.stone@colorado.edu>
2017-11-29 23:11:30 -0700

    Adding basics for website

--- web/index.js
+++ web/index.js
@@ -4,6 +4,9 @@ var path = require('path');
 
 var port = process.env.PORT || 8080;
 
+app.use('/skeleton', express.static(path.join(__dirname, 'node_modules/skeleton-css/css')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
