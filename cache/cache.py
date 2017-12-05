9b07c78378158fce2c9df368f2229b0dfe11cbfb
Cathlyn Stone <cathlyn.stone@colorado.edu>
2017-12-04 23:36:09 -0700

    adding ability to cache trained models

---
+++ cache/cache.py
@@ -0,0 +1,22 @@
+import pickle
+from pathlib import Path
+
+class Cache:
+
+    def __init__(self):
+        pass
+
+    # Assume cache exists for classifier if there is a pkl file for it
+    def has(self, classifier):
+        my_file = Path(self._get_filename(classifier))
+        return my_file.is_file()
+
+    def get(self, classifier):
+        return pickle.load(open(self._get_filename(classifier), "rb"))
+
+    def set(self, classifier):
+        f = open(self._get_filename(classifier), "wb")
+        pickle.dump(classifier, f)
+
+    def _get_filename(self, classifier):
+        return './cache/%s.pkl' % classifier.name
