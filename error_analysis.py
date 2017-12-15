71853a8d1756e7dee7124ccdbf602e8a734d2b5d
Hunter Wapman <hunter.wapman@gmail.com>
2017-12-14 19:52:40 -0700

    updated report with some error analysis. modified report some

--- error_analysis.py
+++ error_analysis.py
@@ -30,6 +30,8 @@ class ErrorAnalysis:
         correct_counts = [ corrects[l] for l in lengths ]
         total_counts = [ totals[l] for l in lengths ]
 
+        print(lengths)
+
         percents = []
         for c, t in zip(correct_counts, total_counts):
             percent = c/t if c else 0.0
@@ -49,3 +51,30 @@ class ErrorAnalysis:
             info = [total_counts, correct_counts, percents]
             pickle.dump(info, f)
 
+def get_data(graphic, algorithm):
+    path = 'error_analysis/sentence_length/location-%s-%s.pkl' % (graphic, algorithm)
+
+    with open(path, 'rb') as f:
+        _, _, percents = pickle.load(f)
+
+    return percents
+
+def graph_it(algorithm):
+    hom = get_data('homographic', algorithm)
+    hom_lens = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 29, 30, 33, 34, 37, 48, 50]
+
+    het = get_data('heterographic', algorithm)
+    het_lens = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 36 , 40]
+
+    plt.clf()
+    plt.scatter(hom_lens, hom, c='red', label="Homographic")
+    plt.scatter(het_lens, het, c='green', label="Heterographic")
+    plt.xlabel("sentence length")
+    plt.ylabel("accuracy")
+    plt.legend()
+    path = 'error_analysis/sentence_length/location-%s' % algorithm
+    plt.savefig(path)
+
+if __name__ == "__main__":
+    graph_it('window')
+    graph_it('rnn')
